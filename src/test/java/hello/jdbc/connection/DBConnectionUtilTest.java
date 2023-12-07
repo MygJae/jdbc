98a56d39782ca1c434d2aa800c69262cda8456c0
Mygjae <50861514+MygJae@users.noreply.github.com>
2023-12-07 22:52:07 +0900

    JDBC 이해

--- src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
+++ src/test/java/hello/jdbc/connection/DBConnectionUtilTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +9,7 @@ import java.sql.Connection;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class DBConnectionUtilTest {
 
     @Test
